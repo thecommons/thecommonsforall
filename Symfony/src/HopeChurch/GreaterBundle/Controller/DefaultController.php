0071e45605e0a40a13e772a300115b9a4c780e1b
Alistair King <alistair@caida.org>
2013-03-25 20:51:53 -0700

    make symfony at least kinda work!

--- Symfony/src/HopeChurch/GreaterBundle/Controller/DefaultController.php
+++ Symfony/src/HopeChurch/GreaterBundle/Controller/DefaultController.php
@@ -6,9 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($page)
+  private $pages = array(
+			 'home',
+			 'attendance',
+			 'add',
+			 'edit'
+			 );
+  
+  public function indexAction($page, $_format, $_route)
     {
-        return $this->render('HopeChurchGreaterBundle:Default:index.html.twig', 
-			     array('page' => $page));
+      /* if they have asked for 'index', send them to 'home' */
+      if($page == 'index')
+	{
+	  $page = 'home';
+	}		    
+
+      return $this->render("HopeChurchGreaterBundle:Default:$page.$_format.twig", 
+			   array(
+				 'route' => $_route,
+				 'active_page' => $page,
+				 'format' => $_format,
+				 'nav_pages' => $this->pages
+				 ));
     }
 }
--- Symfony/src/HopeChurch/GreaterBundle/HopeChurchGreaterBundle.php
+++ Symfony/src/HopeChurch/GreaterBundle/HopeChurchGreaterBundle.php
@@ -6,4 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class HopeChurchGreaterBundle extends Bundle
 {
+
 }
