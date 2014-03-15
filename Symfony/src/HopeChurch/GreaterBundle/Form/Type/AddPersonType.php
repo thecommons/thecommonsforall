e923ebebd5aad19147f162c5569fc71c3817120f
Alistair King <alistair@caida.org>
2014-03-14 22:43:59 -0700

    first step toward having mentor list filtered by tstage_mentorroles

--- Symfony/src/HopeChurch/GreaterBundle/Form/Type/AddPersonType.php
+++ Symfony/src/HopeChurch/GreaterBundle/Form/Type/AddPersonType.php
@@ -3,14 +3,22 @@
 namespace HopeChurch\GreaterBundle\Form\Type;
 
 use HopeChurch\GreaterBundle\Entity\PersonRepository;
+use HopeChurch\GreaterBundle\Entity\Person;
+use HopeChurch\GreaterBundle\Entity\TransformationalStage;
+
+use Doctrine\ORM\EntityRepository;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
 
 class AddPersonType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
+    $factory = $builder->getFormFactory();
+
     $builder->add('nameFirst', 'text', array(
                                              'label' => 'First Name'
                                              ));
@@ -59,14 +67,6 @@ class AddPersonType extends AbstractType
 					   'multiple'     => true,
 					   'expanded'     => false
 					   ));
-
-    $builder->add('transformationalStage', 'entity',
-		  array(
-			'label' => 'Transformational Stage',
-			'required' => true,
-			'class' => 'HopeChurchGreaterBundle:TransformationalStage',
-			'property' => 'name'
-			));
 
     $builder->add('baptized', 'checkbox', array(
 						'label' => 'Baptized',
@@ -123,6 +123,77 @@ class AddPersonType extends AbstractType
                                                 'required' => false
                                                 ));
 
+    $builder->add('transformationalStage', 'entity',
+		  array(
+			'label' => 'Transformational Stage',
+			'required' => true,
+			'class' => 'HopeChurchGreaterBundle:TransformationalStage',
+			'property' => 'name'
+			));
+
+    $refreshMentor = function ($form, $tstage) use ($factory) {
+      $form->add($factory->createNamed('mentor', 'entity', null,
+				       array(
+					     'class' => 'HopeChurchGreaterBundle:Person',
+					     'empty_value'   => '-- Select Mentor --',
+					     'required'      => true,
+					     'label'         => 'Mentor',
+					     'auto_initialize' => false,
+					     'query_builder' =>
+					     function (EntityRepository $repository) use ($tstage) {
+					       $qb = $repository
+						 ->createQueryBuilder('p')
+						 ->innerJoin('p.roles', 'proles')
+						 ->innerJoin('proles.transformationalStages',
+							     'pts');
+
+					       if($tstage instanceof TransformationalStage) {
+						 $qb = $qb
+						   ->where('pts = :tstage')
+						   ->setParameter('tstage', $tstage);
+					       } elseif(is_numeric($tstage)) {
+						 $qb = $qb
+						   ->where('pts.id = :tstage_id')
+						   ->setParameter('tstage_id', $tstage);
+					       } else {
+						 $qb = $qb->where('pts.id = 1'); //nothing
+					       }
+
+					       return $qb;
+					     }
+					     )
+				       )
+		 );
+    };
+
+    $builder->addEventListener(FormEvents::PRE_SET_DATA,
+			       function (FormEvent $event) use ($refreshMentor) {
+				 $form = $event->getForm();
+				 $data = $event->getData();
+
+				 if($data == null) {
+				   //As of beta2, when a form is created
+				   //setData(null) is called first
+				   $refreshMentor($form, null);
+				 } else if($data instanceof Person) {
+				   $refreshMentor($form,
+						  $data->getTransformationalStage());
+				 } else {
+				   var_dump($data);
+				 }
+			       });
+
+    $builder->addEventListener(FormEvents::PRE_BIND,
+			       function (FormEvent $event) use ($refreshMentor) {
+				 $form = $event->getForm();
+				 $data = $event->getData();
+
+				 if(array_key_exists('transformationalStage', $data)) {
+				   $refreshMentor($form, $data['transformationalStage']);
+				 }
+			       });
+
+    /*
     $builder->add('mentor',
                   'entity',
                   array(
@@ -138,6 +209,8 @@ class AddPersonType extends AbstractType
                         'required' => false
 			)
 		  );
+    */
+
   }
 
   public function getName()
