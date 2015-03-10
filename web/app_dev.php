8859a2b154b6e4218768e988ed32e844aa7e85b0
Alistair King <alistair@caida.org>
2015-03-10 10:31:11 -0700

    upgrade AppKernel and app_dev to work with symfony 2.6

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,20 +11,24 @@ use Symfony\Component\HttpFoundation\Request;
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('70.166.81.247', '127.0.0.1',
-    'fe80::1', '::1', '192.172.226.97', '68.8.31.76', '72.207.28.40', '132.249.65.101'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'],
+        array('127.0.0.1',
+            'fe80::1', '::1',
+            '192.172.226.97', // gibi.caida.org
+            '68.8.31.76', // whyte.bellstreet.co.nz
+        ))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 Debug::enable();
-require_once __DIR__.'/../app/AppKernel.php';
+
+require_once __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
