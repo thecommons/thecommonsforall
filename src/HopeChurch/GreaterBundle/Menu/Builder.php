8d1b1ed6e1535a55445a0ae4e7ffe688efe949ee
Alistair King <alistair@caida.org>
2015-03-10 10:23:49 -0700

    upgrade symfony to 2.6

--- src/HopeChurch/GreaterBundle/Menu/Builder.php
+++ src/HopeChurch/GreaterBundle/Menu/Builder.php
@@ -3,75 +3,81 @@
 namespace HopeChurch\GreaterBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class Builder
 {
-  private $factory;
+    private $factory;
+    protected $authorizationCheckerInterface;
 
-  /**
-   * @param FactoryInterface $factory
-   */
-  public function __construct(FactoryInterface $factory)
-  {
-    $this->factory = $factory;
-  }
+    /**
+     * @param FactoryInterface $factory
+     * * @param AuthorizationCheckerInterface $authorizationCheckerInterface
+     */
+    public function __construct(FactoryInterface $factory,
+                                AuthorizationCheckerInterface $authorizationCheckerInterface)
+    {
+        $this->factory = $factory;
+        $this->authorizationCheckerInterface = $authorizationCheckerInterface;
+    }
 
-  public function topNavMenu()
-  {
-    $menu = $this->factory->createItem('root');
+    public function topNavMenu()
+    {
+        $menu = $this->factory->createItem('root');
 
-    $menu->addChild('home',
-		    array(
-			  'route' => 'hope_church_greater',
-			  'routeParameters' =>
-			  array(
-				'page' => 'home'
-				)
-			  )
-		    );
+        // to check for authorized roles:
+        // $this->authorizationCheckerInterface->isGranted('ROLE_USER')
 
-    $menu->addChild('attendance',
-		    array(
-			  'route' => 'hope_church_greater',
-			  'routeParameters' =>
-			  array(
-				'page' => 'attendance'
-				)
-			  )
-		    );
+        $menu->addChild('home',
+            array(
+                'route' => 'hope_church_greater',
+                'routeParameters' =>
+                    array(
+                        'page' => 'home'
+                    )
+            )
+        );
 
-    $menu->addChild('people');
-    $menu['people']->addChild('browse', array('route' => 'person'));
-    $menu['people']->addChild('add', array('route' => 'person_new'));
-    //$menu['people']->addChild('edit', array('route' => 'person_edit'));
+        $menu->addChild('attendance',
+            array(
+                'route' => 'hope_church_greater',
+                'routeParameters' =>
+                    array(
+                        'page' => 'attendance'
+                    )
+            )
+        );
 
-    /** Everything below here is crap and needs fixing **/
-    $menu->addChild('families');
-    $menu['families']->addChild('add',
-				array(
-				      'route' => 'hope_church_greater',
-				      'routeParameters' =>
-				      array(
-					    'page' => 'add',
-					    'subpage' => 'family'
-					    )
-				      )
-				);
+        $menu->addChild('people');
+        $menu['people']->addChild('browse', array('route' => 'person'));
+        $menu['people']->addChild('add', array('route' => 'person_new'));
+        //$menu['people']->addChild('edit', array('route' => 'person_edit'));
 
-    $menu->addChild('community groups');
-    $menu['community groups']->addChild('add',
-				array(
-				      'route' => 'hope_church_greater',
-				      'routeParameters' =>
-				      array(
-					    'page' => 'add',
-					    'subpage' => 'communitygroup'
-					    )
-				      )
-				);
+        /** Everything below here is crap and needs fixing **/
+        $menu->addChild('families');
+        $menu['families']->addChild('add',
+            array(
+                'route' => 'hope_church_greater',
+                'routeParameters' =>
+                    array(
+                        'page' => 'add',
+                        'subpage' => 'family'
+                    )
+            )
+        );
 
-    return $menu;
-  }
+        $menu->addChild('community groups');
+        $menu['community groups']->addChild('add',
+            array(
+                'route' => 'hope_church_greater',
+                'routeParameters' =>
+                    array(
+                        'page' => 'add',
+                        'subpage' => 'communitygroup'
+                    )
+            )
+        );
+
+        return $menu;
+    }
 }
